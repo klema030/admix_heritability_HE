4b4a458de1224e07734465c5d4dc8428cbcbef65
Nicole Kleman <138905219+klema030@users.noreply.github.com>
2024-02-23 15:29:37 -0600

    Add files via upload

--- code/AdjustedHE_plot.R
+++ code/AdjustedHE_plot.R
@@ -1,11 +1,10 @@
-# plot vg results from 3 estimators (alternative to GCTA-GREML analysis): 
-# Dicker1, Dicker2 (Dicker 2014), and Haseman-Elston regression analysis 
+# plot vg results Haseman-Elston regression analysis 
 
 # load summary stat, average of 10 reps 
 library(data.table)
 
-DickerHE_HI=fread("HEreg_HI.txt", header=T)
-DickerHE_CGF=fread("HEreg_CGF.txt", header=T)
+HE_HI=fread("HEreg_HI.txt", header=T)
+HE_CGF=fread("HEreg_CGF.txt", header=T)
 
 HE_HI_zero=fread("HE_HI_zero_summary.txt", header=T)
 HE_CGF_zero=fread("HE_CGF_zero_summary.txt", header=T)
@@ -75,7 +74,7 @@ fig4color=function(data){
   library(ggplot2)
   ggplot() +
     geom_line(data=data, linewidth=0.9, alpha = 0.65, #transparent this line
-              aes(x=t, y = tau2_dicker2,                               
+              aes(x=t, y = tau2_2,                               
                   linetype = "obs",
                   group=interaction(P, cov), 
                   color=interaction(P, cov))) +
@@ -110,55 +109,56 @@ fig4color=function(data){
                                  override.aes = list(linewidth = 2)),  # thicken the line in legend
            linetype = "none" 
     )}
-HI_color=fig4color(DickerHE_HI)
+HI_color=fig4color(HE_HI)
 
 
-
-# tau2 from Dicker estimator is Vg
-HIa_wo_1=fig4A(data=DickerHE_HI, 
-               yobs = DickerHE_HI$vg_HEreg_cov, 
-               yexp = DickerHE_HI$vg_123,
-               ylab = expression(hat(V)[g]),
-               title="Hybrid Isolation - Dicker1", 
-               legend.position = "none") 
-
-HIa_wo_2=fig4A(data=DickerHE_HI, 
-               yobs = DickerHE_HI$tau2_dicker2, 
-               yexp = DickerHE_HI$VG_123,
-               ylab = expression(hat(V)[g]),
-               title="Hybrid Isolation - Dicker2", 
-               legend.position = "none") 
-
-# the HEreg RESULT vg_HEreg is VG/VP
-# vg_HEreg_new is the result of vg_HEreg * var.pheno, ie VG
-HIa_HE=fig4A(data=DickerHE_HI, 
-             yobs = DickerHE_HI$vg_HEreg_noa, 
-             yexp = DickerHE_HI$var.prs.geno,
+# HE pos/neg unadjusted manual
+HIa_HE=fig4A(data=HE_HI, 
+             yobs = HE_HI$vg_HEreg_noa, 
+             yexp = HE_HI$var.prs.geno,
              ylab = expression(hat(V)[g]),
              title="Hybrid Isolation - HE",
              legend.position = "none") 
 
-HIa_gcta=fig4A(data=DickerHE_HI, 
-                yobs = DickerHE_HI$vg_HEreg_new, 
-                yexp = DickerHE_HI$var.prs.geno,
+CGFa_HE=fig4A(data=HE_CGF, 
+              yobs = HE_CGF$vg_HEreg_noa, 
+              yexp = HE_CGF$var.prs.geno,
+              ylab = expression(hat(V)[g]),
+              title="Continuous Gene Flow - HE")
+
+# the HEreg RESULT vg_HEreg_gcta is VG/VP
+# vg_HEreg_new is the result of vg_HEreg * var.pheno, ie VG
+#HE pos/neg GCTA
+HIa_gcta=fig4A(data=HE_HI, 
+                yobs = HE_HI$vg_HEreg_new, 
+                yexp = HE_HI$var.prs.geno,
                 ylab = expression(hat(V)[g]),
                 title="Hybrid Isolation - HE GCTA",
                legend.position = "none") 
 
-CGFa_gcta=fig4A(data=DickerHE_CGF, 
-                yobs = DickerHE_CGF$vg_HEreg_new, 
-                yexp = DickerHE_CGF$var.prs.geno,
+CGFa_gcta=fig4A(data=HE_CGF, 
+                yobs = HE_CGF$vg_HEreg_new, 
+                yexp = HE_CGF$var.prs.geno,
                 ylab = expression(hat(V)[g]),
                 title="Continuous Gene Flow - HE GCTA") 
 
-CGFa_HE=fig4A(data=DickerHE_CGF, 
-              yobs = DickerHE_CGF$vg_HEreg_noa, 
-              yexp = DickerHE_CGF$var.prs.geno,
-              ylab = expression(hat(V)[g]),
-              title="Continuous Gene Flow - HE") 
+
+#HE pos/neg adjusted manual
+HI_HE_adj=fig4A(data=HE_HI, 
+                yobs = HE_HI$vg_HEreg_a, 
+                yexp = HE_HI$var.prs.geno,
+                ylab = expression(hat(V)[g]),
+                title="Hybrid Isolation - HE adjusted",
+                legend.position = "none") 
+
+CGF_HE_adj=fig4A(data=HE_CGF, 
+                 yobs = HE_CGF$vg_HEreg_a, 
+                 yexp = HE_CGF$var.prs.geno,
+                 ylab = expression(hat(V)[g]),
+                 title="Continous Gene Flow - HE adjusted") 
 
 
-# plot vg estimate and expected 
+# plot vg estimate and expected for neutral trait
 fig4Azero=function(data, yobs, yexp, ylab, title, legend.position="right"){
   library(ggplot2)
   ggplot() +
@@ -232,9 +232,9 @@ fig4colorzero=function(data){
                                  override.aes = list(linewidth = 2)),  # thicken the line in legend
            linetype = "none" 
     )}
-HI_color=fig4colorzero(HE_HI_zero)
+HI_color_zero=fig4colorzero(HE_HI_zero)
 
-#Zeros adjusted
+#HE Zeros adjusted manual
 HI_HE_zero=fig4Azero(data=HE_HI_zero,
                  yobs = HE_HI_zero$vg_zero_adj,
                  yexp = HE_HI_zero$vg_123,
@@ -249,7 +249,7 @@ CGF_HE_zero=fig4Azero(data=HE_CGF_zero,
                  ylab = expression(hat(V)[g]),
                  title="Continuous Gene Flow - HE Adjusted")
 
-#zeros unadjusted
+#HE zeros unadjusted manual
 HI_HE_zero_noa=fig4Azero(data=HE_HI_zero,
                  yobs = HE_HI_zero$vg_zeros_noa,
                  yexp = HE_HI_zero$vg_123,
@@ -263,7 +263,7 @@ CGF_HE_zero_noa=fig4Azero(data=HE_CGF_zero,
                   ylab = expression(hat(V)[g]),
                   title="Continuous Gene Flow - HE")
 
-#zeros unadjusted from gcta
+#HE zeros unadjusted from gcta
 HI_HE_zero_gcta=fig4Azero(data=HE_HI_zero,
                      yobs = HE_HI_zero$vg_HEreg_new,
                      yexp = HE_HI_zero$vg_123,
@@ -277,19 +277,6 @@ CGF_HE_zero_gcta=fig4Azero(data=HE_CGF_zero,
                       ylab = expression(hat(V)[g]),
                       title="Continuous Gene Flow - HE GCTA")
 
-#HE cov adjusted
-HI_HE_adj=fig4A(data=DickerHE_HI, 
-             yobs = DickerHE_HI$vg_HEreg_a, 
-             yexp = DickerHE_HI$var.prs.geno,
-             ylab = expression(hat(V)[g]),
-             title="Hybrid Isolation - HE adjusted",
-             legend.position = "none") 
-
-CGF_HE_adj=fig4A(data=DickerHE_CGF, 
-                yobs = DickerHE_CGF$vg_HEreg_a, 
-                yexp = DickerHE_CGF$var.prs.geno,
-                ylab = expression(hat(V)[g]),
-                title="Continous Gene Flow - HE adjusted") 
 
 library(ggpubr)
 #zeros
@@ -299,10 +286,12 @@ plt_zero=ggarrange(HI_HE_zero_noa, CGF_HE_zero_noa,
               ncol = 2, nrow = 3, 
               labels = c("A", "", "B", "", "C", ""),
               align = c("h")) %>% # to move the legend closer 
-  gridExtra::grid.arrange(get_legend(HI_color), 
+  gridExtra::grid.arrange(get_legend(HI_color_zero), 
                           #heights = unit(c(160, 10), "mm")
                           heights = unit(c(8, 0.8), "in")
   )   
+
+#save output zero
 ggsave("HE_vg_adj_zero.png", plot=plt_zero,
        width = 9, height = 9, dpi = 300, units = "in", device='png')
 
@@ -316,13 +305,10 @@ plt_cov=ggarrange(HIa_HE, CGFa_HE,
   gridExtra::grid.arrange(get_legend(HI_color), 
                           #heights = unit(c(160, 10), "mm")
                           heights = unit(c(8, 0.8), "in")
-  )   
+  )  
+
+#save output pos/neg
 ggsave("HE_vg_adjusted.png", plot=plt_cov,
        width = 9, height = 9, dpi = 300, units = "in", device='png')
 
-#output files
-write.table(DickerHE_HI, "HEreg_HI.txt", sep = "\t", quote = F, row.names = F)
-write.table(DickerHE_CGF, "HEreg_CGF.txt", sep = "\t", quote = F, row.names = F)
 
-write.table(HI_var_pheno, "HEreg_HI_zero_w.txt", sep = "\t", quote = F, row.names = F)
-write.table(CGF_var_pheno, "HEreg_CGF_zero_w.txt", sep = "\t", quote = F, row.names = F)
